a04564e514d17c1fa050f767c66895a5e6079f92
Massimiliano Sgavioli <maxs@virgilio.it>
2016-02-03 14:48:41 +0100

    quantità

--- test.php
+++ test.php
@@ -1,13 +1,13 @@
 <?php
 session_start();
-unset($_SESSION['carrello']);
+//unset($_SESSION['carrello']);
 print_r($_SESSION['carrello']);
 if (isset($_SESSION['carrello'])) {
   $carrello = $_SESSION['carrello'];
   var_dump(count($carrello));
-  for ($i=0; $i < count($carrello) ; $i++) {
-    var_dump($carrello[$i]['prodotto']['codice']);
-    var_dump($carrello[$i]['quantita']);
+  foreach ($carrello as $rigacarrello){
+    echo $rigacarrello['prodotto']['codice'];
+    //var_dump($carrello[$i]['quantita']);
   }
 
 
